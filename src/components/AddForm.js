788e050e16d09ae5eb6111ee3a35b87cb51ba679
imen-lakrib <lak12imen@gmail.com>
2023-02-02 20:56:02 +0100

    add data

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,22 +1,42 @@
-import React from 'react';
-
+import React, { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../store/bookSlice';
 const Addform = () => {
+
+  //refs: 
+  const title= useRef(null)
+  const price= useRef(null)
+  const description= useRef(null)
+  const dispatch = useDispatch(state=> state.books)
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const bookData= {
+      title: title.current.value,
+      price : price.current.value,
+      description: description.current.value
+    }
+
+    dispatch(addBook(bookData))
+  }
+
   return (
     <div className='row'>
       <div className='col-6 offset-3 mt-3'>
         <h2>Insert Book</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='form-group'>
             <label htmlFor='title'>Title</label>
-            <input type='text' className='form-control' id='title' required />
+            <input ref={title} type='text' className='form-control' id='title' required />
           </div>
           <div className='form-group'>
             <label htmlFor='price'>Price</label>
-            <input type='number' className='form-control' id='price' required />
+            <input ref={price} type='number' className='form-control' id='price' required />
           </div>
           <div className='form-group'>
             <label htmlFor='Description'>Description</label>
             <textarea
+            ref={description}
               className='form-control'
               id='Description'
               rows='3'
--- src/store/bookSlice.js
+++ src/store/bookSlice.js
@@ -1,5 +1,6 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
 import axios from 'axios'
+// get data
 export const getBooks = createAsyncThunk('book/getBooks', async(args, thunkAPI)=>{
     const {rejectWithValue} = thunkAPI
     try {
@@ -12,11 +13,33 @@ export const getBooks = createAsyncThunk('book/getBooks', async(args, thunkAPI)=
         
     }
 })
+// add data
+export const addBook = createAsyncThunk('book/addBook', async(bookData, thunkAPI)=>{
+    const {rejectWithValue} = thunkAPI
+    try {
+        const res= await fetch("http://localhost:3009/books", {
+            method: 'POST',
+            body: JSON.stringify(bookData),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+            
+        })
+        const data= await res.json()
+        return data
+        
+    } catch (error) {
+        return rejectWithValue(error.message)
+        
+    }
+})
+
 
 const bookSlice = createSlice({
     name:"book",
     initialState: {books: null, isLoading: false, isError: false},
     extraReducers: {
+        // get data
         [getBooks.pending]: (state, action)=>{
             state.isLoading = true
             state.isError = false
@@ -30,7 +53,22 @@ const bookSlice = createSlice({
             state.isLoading = false
             state.isError = action.payload
 
+        },
+        // add data:
+        [addBook.pending]: (state, action)=>{
+            state.isLoading = true
+            state.isError = false
+        },
+        [addBook.fulfilled]: (state, action)=>{
+            state.isLoading = false
+            state.books.push(action.payload)
+        },
+        [addBook.rejected]: (state, action)=>{
+            state.isLoading = false
+            state.isError = action.payload
+
         }
+
     }
 })
 
